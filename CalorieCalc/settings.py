c6d1b1bd296b822311c51c406dda823980f5f590
cuplyk <ursachi.g@mail.ru>
2022-07-13 08:25:31 +0200

    delete

--- CalorieCalc/settings.py
+++ CalorieCalc/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='ursachi.g@mail.ru'
-EMAIL_HOST_PASSWORD='kotik1234'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
